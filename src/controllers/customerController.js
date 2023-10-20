8494eefe14d99342a4870988d4deb165fb5ea1e7
Laquinh <anderaginaga2@gmail.com>
2023-10-20 14:27:20 +0200

    Actually use controller

---
+++ src/controllers/customerController.js
@@ -0,0 +1,7 @@
+const controller = {};
+
+controller.list = (req, res) => {
+    res.send('Hello World!');
+};
+
+module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const customerController = require('../controllers/customerController');
+
+router.get('/', customerController.list);
 
 module.exports = router;
