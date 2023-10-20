2bfd1c133995350f539cba02dbca1985f1412c02
Laquinh <anderaginaga2@gmail.com>
2023-10-20 13:44:53 +0200

    use morgan middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const app = express();
 
@@ -6,6 +7,9 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// middlewares
+app.use(morgan('dev'));
 
 app.listen(app.get('port'), () => {
     console.log('Server is up on port 3000');
