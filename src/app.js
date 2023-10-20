63a3a522d8e4c9300de40e5b707574320e37f98a
Laquinh <anderaginaga2@gmail.com>
2023-10-20 14:22:36 +0200

    Create customer router and controller
    and print 'Hello World!' :)

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 
 require('dotenv').config();
 
+// importing routes
+const customerRoutes = require('./routes/customer');
+
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
@@ -21,6 +24,12 @@ app.use(myConnection(mysql, {
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE
 }, 'single'));
+
+// routes
+app.use('/', customerRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
     console.log('Server is up on port 3000');
