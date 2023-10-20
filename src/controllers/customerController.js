0b7ba86c98e80c0eeede4ca745142e49ebf6c090
Laquinh <anderaginaga2@gmail.com>
2023-10-20 17:39:00 +0200

    Edit customers

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -26,6 +26,38 @@ controller.save = (req, res) => {
     });
 };
 
+controller.edit = (req, res) => {
+    console.log("edit");
+    const {id} = req.params;
+    
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer WHERE id = ?', [id], (err, customer) => {
+            if (err) {
+                res.json(err);
+            }
+            console.log(customer[0]);
+            res.render('customer_edit', {
+                data: customer[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    console.log("update");
+    const {id} = req.params;
+    const newCustomer = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE customer SET ? WHERE id = ?', [newCustomer, id], (err, rows) => {
+            if (err) {
+                res.json(err);
+            }
+            res.redirect('/');
+        });
+    });
+};
+
 controller.delete = (req, res) => {
     const {id} = req.params;
 
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -5,6 +5,8 @@ const customerController = require('../controllers/customerController');
 
 router.get('/', customerController.list);
 router.post('/add', customerController.save);
-router.get('/delete/:id', customerController.delete)
+router.get('/edit/:id', customerController.edit);
+router.post('/edit/:id', customerController.update);
+router.get('/delete/:id', customerController.delete);
 
 module.exports = router;
